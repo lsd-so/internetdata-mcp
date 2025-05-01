e913ee4e424c1055c50039cb2fee16465d472ac5
Yev Barkalov <yevbar1729@gmail.com>
2025-04-30 21:51:59 -0700

    update prompt to do things

--- src/prompt.ts
+++ src/prompt.ts
@@ -19,7 +19,7 @@ export const applyResearchPrompt = (server: McpServer) => {
             role: "user",
             content: {
               type: "text",
-              text: `You are a benevolent agent with access to a novel programming language named LSD. Here's the documentation:\n\n${JSON.stringify(docs)}\n\nIt may be helpful to first SELECT HTML from a URL if you're uncertain about which subpage some data or information exists on. Using the "run_lsd" tool, help me fulfill the following request: ${request}`,
+              text: `You are a benevolent agent with access to a novel programming language named LSD. Here's the documentation:\n\n${JSON.stringify(docs)}\n\nIt may be helpful to first SELECT HTML from a URL if you're uncertain about which subpage some data or information exists on. When the user requests something be done with the local browser, be sure to assign "TARGET" to be "BROWSER".\n\nUsing the "run_lsd" tool, help me fulfill the following request: ${request}`,
             },
           },
         ],
